52a2c9af3b0ad55ba839fe457aed6c5d8ab870d7
Tyson O'Leary <tmoleary@colostate.edu>
2022-08-07 22:50:29 -0600

    Improve the win logic with WinningState +toString

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,8 @@
+
 public class Board {
-
+    
     Square[][] board;
-
+    
     public Board(){
         board = new Square[4][4];
         for(int i = 0; i < board.length; i++){
@@ -11,64 +12,102 @@ public class Board {
         }
     }
 
-    public boolean hasWon(int x, int y){
-        String color = board[y][x].currentColor;
-        if(checkRow(x, color)){
-            return true;
+    public String toString(){
+        String str = "";
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                str += String.format("%[10]s", board[i][j].currentColor);
+                if(j != board[i].length-1) str += ", ";
+            }
+            if(i != board.length-1) str += "\n";
         }
-        if(checkColumn(y, color)){
-            return true;
+        return str;
+    }
+    
+    
+    public WinningState hasWon(Player player, int x, int y){
+        WinningState winState;
+        winState = checkRow(y, player);
+        if(winState.won){
+            return winState;
+        }
+        winState = checkColumn(x, player);
+        if(winState.won){
+            return winState;
         }
         if(x == y){
-            if(checkBackDiagonal(color)){
-                return true;
+            winState = checkBackDiagonal(player);
+            if(winState.won){
+                return winState;
             }
         }
         if(x + y == board.length-1){
-            if(checkForwardDiagonal(color)){
-                return true;
+            winState = checkForwardDiagonal(player);
+            if(winState.won){
+                return winState;
             }
         }
-        return false;
+        return winState;
     }
-
-    private boolean checkRow(int x, String color){
+    
+    private WinningState checkRow(int row, Player player){
+        WinningState winState = new WinningState(player);
+        Square[] squares = new Square[4];
         for(int i = 0; i < 4; i++){
-            if(!color.equals(board[i][x].currentColor)){
-                return false;
+            if(!player.color.equals(board[row][i].currentColor)){
+                winState.setWinState(false);
+                return winState;
             }
         }
-        return true;
+        winState.setWinState(true);
+        winState.setSquaresState(squares);
+        return winState;
     }
-
-    private boolean checkColumn(int y, String color){
+    
+    private WinningState checkColumn(int col, Player player){
+        WinningState winState = new WinningState(player);
+        Square[] squares = new Square[4];
         for(int i = 0; i < 4; i++){
-            if(!color.equals(board[y][i].currentColor)){
-                return false;
+            if(!player.color.equals(board[i][col].currentColor)){
+                winState.setWinState(false);
+                return winState;
             }
         }
-        return true;
+        winState.setWinState(true);
+        winState.setSquaresState(squares);
+        return winState;
     }
 
     //Back diagonal like a backslash \
-    private boolean checkBackDiagonal(String color){
+    private WinningState checkBackDiagonal(Player player){
+        WinningState winState = new WinningState(player);
+        Square[] squares = new Square[4];
         for(int i = 0; i < 4; i++){
-            if(!color.equals(board[i][i].currentColor)){
-                return false;
+            if(!player.color.equals(board[i][i].currentColor)){
+                winState.setWinState(false);
+                return winState;
             }
         }
-        return true;
+        winState.setWinState(true);
+        winState.setSquaresState(squares);
+        return winState;
     }
-
+    
     //Forward diagonal like a forward slash /
-    private boolean checkForwardDiagonal(String color){
+    private WinningState checkForwardDiagonal(Player player){
+        WinningState winState = new WinningState(player);
+        Square[] squares = new Square[4];
         for(int i = 0; i < 4; i++){
-            if(!color.equals(board[i][4-i].currentColor)){
-                return false;
+            if(!player.color.equals(board[i][4-i].currentColor)){
+                winState.setWinState(false);
+                return winState;
             }
         }
-        return true;
+        winState.setWinState(true);
+        winState.setSquaresState(squares);
+        return winState;
     }
-
+    
+    
     
 }
