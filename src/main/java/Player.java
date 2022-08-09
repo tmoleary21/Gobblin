186a3365dc733e362d7aa9c534c4d8bdad8b82c8
DGaron <dgaron@colostate.edu>
2022-08-09 13:28:49 -0600

    Add constructor Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,18 @@
 public class Player {
     String color;
+    String name;
+
+    public Player(String color, String name) {
+        this.color = color;
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
