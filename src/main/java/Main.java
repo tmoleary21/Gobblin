aaf234af96e72a6a8e262fbd2173d30c71da4bd2
Tyson O'Leary <tmoleary@colostate.edu>
2022-08-11 00:20:33 -0600

    Fix leftover errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,51 +1,7 @@
 public class Main {
     
     public static void main(String[] args){
-        testWinning();
-    }
-    
-    public static void testWinning(){
         Player player1 = new Player("Player 1", "white");
         Player player2 = new Player("Player 2", "black");
-
-        //Back diagonal
-        Board board = new Board();
-        board.board[0][0].currentColor = "red";
-        board.board[1][1].currentColor = "red";
-        board.board[2][2].currentColor = "red";
-        board.board[3][3].currentColor = "red";
-        System.out.println(board.toString());
-        WinningState winState = board.hasWon(player, 1, 1);
-        System.out.println("Win? " + winState.won);
-        
-        //Forward diagonal
-        board = new Board();
-        board.board[0][3].currentColor = "red";
-        board.board[1][2].currentColor = "red";
-        board.board[2][1].currentColor = "red";
-        board.board[3][0].currentColor = "red";
-        System.out.println(board.toString());
-        winState = board.hasWon(player, 2, 1);
-        System.out.println("Win? " + winState.won);
-
-        //Row
-        board = new Board();
-        board.board[2][0].currentColor = "red";
-        board.board[2][1].currentColor = "red";
-        board.board[2][2].currentColor = "red";
-        board.board[2][3].currentColor = "red";
-        System.out.println(board.toString());
-        winState = board.hasWon(player, 2, 2);
-        System.out.println("Win? " + winState.won);
-
-        //Column
-        board = new Board();
-        board.board[0][1].currentColor = "red";
-        board.board[1][1].currentColor = "red";
-        board.board[2][1].currentColor = "red";
-        board.board[3][1].currentColor = "red";
-        System.out.println(board.toString());
-        winState = board.hasWon(player, 1, 3);
-        System.out.println("Win? " + winState.won);
     }
 }
